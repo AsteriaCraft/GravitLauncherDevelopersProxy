8dda1399361ad7adbb9ed5a4852eb4f52cdde97e
MaxNomad <maxnomad123@gmail.com>
2023-01-05 12:33:52 +0200

    add input settings

--- index.js
+++ index.js
@@ -1,21 +1,70 @@
+const fs = require('fs');
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-require('dotenv').config()
-
-console.log(`Gravit Launcher Proxy started at localhost:${process.env.APP_PORT}`)
-
-const wsProxy = createProxyMiddleware({
-  target: process.env.LAUNCHER_WS,
-  changeOrigin: true, 
-  ws: true, 
-  logger: console,
-});
+const readline = require('readline');
 
 const app = express();
-app.use(wsProxy);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-app.use('/', createProxyMiddleware({ target: process.env.LAUNCHER_WEB, changeOrigin: true , logger: console}));
+try {
+    fs.statSync('.env');
+    console.log('[APP] .env exists...');
+}
+catch (err) {
+    if (err.code === 'ENOENT') {
+        console.log('[APP] .env does not exist');
+        fs.writeFileSync('.env', `APP_PORT=9274\nLAUNCHER_WS=\nLAUNCHER_WEB=`);
+        console.log('[APP] .env created..');
+    }
+}
+require('dotenv').config()
 
 const server = app.listen(process.env.APP_PORT);
-server.on('upgrade', wsProxy.upgrade);
+const launcher_ws__ = process.env.LAUNCHER_WS;
+const launcher_web__ = process.env.LAUNCHER_WEB;
 
+const appInit = () => {
+    const port = process.env.APP_PORT || 9274;
+    const launcher_ws = process.env.LAUNCHER_WS;
+    const launcher_web = process.env.LAUNCHER_WEB;
+    try {
+        const wsProxy = createProxyMiddleware({
+            target: launcher_ws,
+            changeOrigin: true,
+            ws: true,
+            logger: console,
+        });
+
+        app.use(wsProxy);
+
+        app.use('/', createProxyMiddleware({ target: launcher_web, changeOrigin: true, logger: console }));
+
+        server.on('upgrade', wsProxy.upgrade);
+        console.log(`[APP] Gravit Launcher Proxy started at localhost:${port}`)
+
+    } catch (e) {
+        console.log(`[ERROR] Init error: ${e}`)
+    }
+}
+
+if (launcher_ws__ && launcher_web__) {
+    appInit();
+} else {
+    console.log(`[APP] Enter config values`);
+    rl.question('[INPUT] Launcher WS: ', (launcher_ws_) => {
+        rl.question('[INPUT] Launcher WEB: ', (launcher_web_) => {
+            fs.writeFileSync('.env', `APP_PORT=9274\nLAUNCHER_WS=${launcher_ws_}\nLAUNCHER_WEB=${launcher_web_}`);
+            process.env['LAUNCHER_WS'] = launcher_ws_;
+            process.env['LAUNCHER_WEB'] = launcher_web_;
+            rl.close();
+        });
+    });
+
+    rl.on('close', () => {
+        console.log('[INSTALL] Prepare configuration.....');
+        appInit();
+    });
+}
